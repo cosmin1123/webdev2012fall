c3186e682efed5547052dd2937b9063b55aa9e4a
cosmin <cosmin@cosmin-Aspire-7750G.(none)>
2012-11-03 10:48:35 +0200

    RED: test_responds_to_turn

--- tictactoeplayer_test.rb
+++ tictactoeplayer_test.rb
@@ -8,4 +8,9 @@ class TestPlayer < Test::Unit::TestCase
     functionality_implemented = true
     assert( functionality_implemented, failure_message = "Functionality not implemented" )
   end
+
+def test_responds_to_turn
+    tttp = TicTacToePlayer.new
+    assert_respond_to( tttp, :turn, failure_message = "An instance of TicTacToePlayer does not respond to turn" )
+  end
 end
