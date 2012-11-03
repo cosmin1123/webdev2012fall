884d9d96d480c3a13cf1d113000493d57c1535c7
cosmin <cosmin@cosmin-Aspire-7750G.(none)>
2012-11-03 11:57:29 +0200

    GREEN: test_turn_not_nil

--- tictactoeplayer_test.rb
+++ tictactoeplayer_test.rb
@@ -1,8 +1,29 @@
 require 'test/unit'
 
 class TicTacToePlayer
+	BOARD = [:a1, :a2, :a3, :b1, :b2, :b3, :c1, :c2, :c3]
+	WINNING_COMBINATIONS = [[:a1, :a2, :a3],
+                          [:b1, :b2, :b3],
+                          [:c1, :c2, :c3],
+                          [:a1, :b1, :c1],
+                          [:a2, :b2, :c2],
+                          [:a3, :b3, :c3],
+                          [:a1, :b2, :c3],
+                          [:c1, :b2, :a3]]
+
+	def initialize
+		@who_moves_next = 1		
+		@owned_by_x     = []
+		@owned_by_zero  = []
+ 	end
+  
 	def turn(input)
-	
+		if @who_moves_next == 1
+			return	@owned_by_x << (BOARD - @owned_by_x - @owned_by_zero).sample
+    	else
+			@owned_by_zero << (BOARD - @owned_by_x - @owned_by_zero).sample
+		end
+		@who_moves_next = 0		
 	end
 end
 
